6a7644c877aaaea82739eef67c356a3e89620286
Sirui Yao <20095250@mail.wit.ie>
2023-11-15 15:48:49 +0000

    add people details test

--- movies/cypress/e2e/peoplepage.cy.js
+++ movies/cypress/e2e/peoplepage.cy.js
@@ -1,3 +1,5 @@
+let PeoplePage;
+let getPeopleDetail;
 describe('People Page', () => {
     it('shows spinner and then people list', () => {
       cy.intercept('GET', 'https://api.themoviedb.org/3/person/popular*', {
@@ -11,5 +13,22 @@ describe('People Page', () => {
         body: { message: 'Internal Server Error' }
       }).as('getPeopleListFail');
       cy.visit('/people');
+    });
+  });
+
+  describe('People Detail Page', () => {
+    it('shows spinner and then people details', () => {
+      cy.intercept('GET', 'https://api.themoviedb.org/3/person/*', {
+      }).as('getPeopleDetail');
+      cy.visit('/people/1');
+      cy.wait('@getPeopleDetail');
+    });
+  
+    it('displays error message on failure', () => {
+      cy.intercept('GET', 'https://api.themoviedb.org/3/person/*', {
+        statusCode: 500,
+        body: { message: 'Internal Server Error' }
+      }).as('getPeopleDetailFail');
+      cy.visit('/people/1');
     });
   });
