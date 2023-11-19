c25b5e057e37aac35ec9f478e94dead782872dd6
Sirui Yao <20095250@mail.wit.ie>
2023-11-19 14:13:59 +0000

    Use custom commands to test login and registration

--- movies/cypress/e2e/firebase.cy.js
+++ movies/cypress/e2e/firebase.cy.js
@@ -1,21 +1,16 @@
-describe('LoginPage', () => {
-    beforeEach(() => {
-      cy.visit('/login');
-    });
-
-    it('allows a user to sign in', () => {
-      cy.get('input[type="email"]').type('1908146704@qq.com');
-      cy.get('input[type="password"]').type('ysr928812');
-      cy.get('form').contains('Login').click();
-      cy.url().should('eq', 'http://localhost:3000/1')
-    });
-
-    it('allows a user to sign up', () => {
-      // avoid conflict with existing users
-      const uniqueEmail = `test_user_${new Date().getTime()}@example.com`;
-      cy.get('input[type="email"]').type(uniqueEmail);
-      cy.get('input[type="password"]').type('password');
-      cy.get('button').contains('Sign Up').click();
-      cy.url().should('eq', 'http://localhost:3000/1')
-    });
+import '../support/commands'
+describe('LoginPage Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login');
   });
+
+  it('allows a user to sign in', () => {
+    cy.login('1908146704@qq.com', 'ysr928812');
+    cy.url().should('include', 'http://localhost:3000/login'); 
+  });
+
+  it('allows a new user to sign up', () => {
+    cy.register('newuser@example.com', 'newpassword123');
+    cy.url().should('include', 'http://localhost:3000/login'); 
+  });
+});
