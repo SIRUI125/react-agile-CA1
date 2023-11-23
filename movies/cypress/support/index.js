0d8b9bad1c661acf3881d756c1f12ea5d4008e78
Sirui Yao <20095250@mail.wit.ie>
2023-11-23 18:43:15 +0000

    fix errors

--- movies/cypress/support/index.js
+++ movies/cypress/support/index.js
@@ -1,6 +1,23 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-  });
-  
+  // we expect a 3rd party library error with message 'list not defined'
+  // and don't want to fail the test so we return false
+  if (err.message.includes('list not defined')) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
+Cypress.on('fail', (error, runnable) => {
+  debugger
+
+  // we now have access to the err instance
+  // and the mocha runnable this failed on
+
+  throw error // throw error to have test still fail
+})
+
+it('calls the "fail" callback when this test fails', () => {
+  // when this cy.get() fails the callback
+  // is invoked with the error
+  cy.get('element-that-does-not-exist')
+})
