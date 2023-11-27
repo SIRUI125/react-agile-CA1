415fdec49335816dd51c789e3fd3242620de0a82
Sirui Yao <144785434+SIRUI125@users.noreply.github.com>
2023-11-27 20:47:01 +0000

    Revert "Pull request"

---
+++ movies/cypress/e2e/firebase.cy.js
@@ -0,0 +1,17 @@
+import '../support/commands'
+describe('LoginPage Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login');
+  });
+
+  it('allows a user to sign in', () => {
+    cy.login('1908146704@qq.com', 'ysr928812');
+    cy.url().should('include', 'http://localhost:3000/login'); 
+  });
+
+  it('allows a new user to sign up', () => {
+    cy.register('newuser@example.com', 'newpassword123');
+    cy.url().should('include', 'http://localhost:3000/login'); 
+  });
+});
+
